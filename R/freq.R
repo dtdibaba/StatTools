353623ef90fe9cef696cdd734e43e1924409b770
Dibaba <ddibaba@uthsc.edu>
2019-12-16 17:14:51 -0600

    --all

---
+++ R/freq.R
@@ -0,0 +1,34 @@
+#'@title Frequency table for a single variable
+
+#' @description This is a frequency table function which prints the frequency
+#'  and related row percentage for a given categorical variable.The function
+#'   creates the count and related row percentages for a given
+#' categorical variables and allows naming the header.
+#' Provide the distinct level names as a list.
+#' @param var  A categorical variable to be analyzed
+#' @param row Row name for the output
+#' @param colnames Level names for the categorical variable
+#' @examples 
+#' #Example
+#' set.seed(1234)
+#' letter=list(sample(letters[1:5], 20, replace=TRUE))
+#' freq( letter, row=c("Letter"), colnames=c("A", "B", "C", "D", "E"))
+#' @export
+freq<-function(var, row, colnames){
+
+  table<-table(var)
+  tab=round(100*prop.table(table),1)
+  ncols=dim(table)
+  for (i in 1:ncols){
+    tab[i]<-paste0(sprintf("%.1f", table[[i]]),'(',tab[[i]],"%",')')
+  }
+  prop<-matrix((c(dimnames(tab[1])[[1]], tab)), byrow=1, ncol=ncols, nrow=1, dimnames = NULL)
+  dimnames(prop)<-list(row, colnames) #The Freq is added as row names.
+  prop<-noquote(prop)
+  return(prop)
+}
+
+
+
+
+
